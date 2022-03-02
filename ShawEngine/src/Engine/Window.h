d3a4a6f9903449b22843f6d2da0500dd9c2dfa84
k_night_zz <619416187@qq.com>
2022-03-02 20:48:38 +0800

    添加Input

--- ShawEngine/src/Engine/Window.h
+++ ShawEngine/src/Engine/Window.h
@@ -40,6 +40,8 @@ namespace ShawEngine {
 		virtual void SetVSync(bool enabled) = 0;
 		virtual bool IsVSync() const = 0;
 
+		virtual void* GetNativeWindow() const = 0;
+
 		//函数声明，不同的平台需要实现各自的Create()，目前有Windows
 		static Window* Create(const WindowProps& props = WindowProps());
 	};
